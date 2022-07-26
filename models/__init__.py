7a63b0d86c9626a331748a2977a81407eb188aa0
sseoreo <njsseo25@gmail.com>
2022-07-26 12:48:06 +0900

    init weigt

--- models/__init__.py
+++ models/__init__.py
@@ -1,36 +1,8 @@
-# from models.single_type import (
-#     SingleType,
-#     SingleTypeAttn,  
-# )
-# from models.single_point import ( 
-#     SinglePoint,
-#     SinglePointAttn, 
-# # )
-
-# from models.single_all import (
-#     SingleAll,
-#     SingleAllAttn,  
-# )
 import torch.nn as nn
-
 from models.seq2seq_type import (
     Seq2SeqType,
     Seq2SeqTypeAttn, 
-    # Seq2SeqPoint, 
-    # Seq2SeqAll
 )
-
-# from models.seq2seq_type_new import (
-#     Seq2SeqTypeNew,
-#     Seq2SeqTypeAttnNew, 
-#     # Seq2SeqPoint, 
-#     # Seq2SeqAll
-# )
-
-# from models.seq2seq_point_ import (
-#     Seq2SeqPoint,
-#     Seq2SeqPointAttn
-# )
 
 from models.seq2seq_point_ce import (
     Seq2SeqPointCE,
@@ -51,41 +23,14 @@ __dict__ = {
     'single_type' : Seq2SeqType,
     'single_type_attn' : Seq2SeqTypeAttn,
     
-    
-
-
     'seq2seq_type': Seq2SeqType,
     'seq2seq_type_attn': Seq2SeqTypeAttn,
     
-    # FIXME
-    # 'seq2seq_type_new': Seq2SeqTypeNew,
-    # 'seq2seq_type_attn': Seq2SeqTypeAttn,
-    
-
-    
-    # 'single_point': Seq2SeqPoint,
-    # 'single_point_attn' : Seq2SeqPointAttn,
-    # 'seq2seq_point': Seq2SeqPoint,
-    # 'seq2seq_point_attn': Seq2SeqPointAttn,
-
-
     'single_point_bce': Seq2SeqPointBCE,
     'single_point_attn_bce' : Seq2SeqPointAttnBCE,
 
     'single_point_ce': Seq2SeqPointCE,
     'single_point_attn_ce' : Seq2SeqPointAttnCE,
-
-    
-
-    
-    'seq2seq_all': Seq2SeqAll,
-    'seq2seq_all_attn': Seq2SeqAllAttn,
-
-
-    
-    # 'single_all': SingleAll,
-    # 'single_all_attn': SingleAllAttn,
-
     }
 
 MODELS = list(__dict__.keys())
---
+++ scripts/train-single-point-attn-bce.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=0 python3.8 main.py \
+    --mode single_point_attn_bce \
+    --dataset july22 \
+    --batch_size 256 \
+    --epoch 300 \
+    --lr 0.001 \
+    --src_len 64 \
+    --embedding_dim 256 \
+    --hidden_dim 512 \
+    --train_interval 10 \
+    --valid_interval 1 \
+    --data_path data \
+    # --debug
